f43e3676503d5675545e68f70a6518446b65e774
odumosukehinde <kodumosu@byupathway.edu>
2025-07-05 21:28:47 +0100

    Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,33 +1,65 @@
+
+
 public static class Arrays
 {
     /// <summary>
-    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
-    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
-    /// integer greater than 0.
+    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  
+    /// For example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.
     /// </summary>
-    /// <returns>array of doubles that are the multiples of the supplied number</returns>
-    public static double[] MultiplesOf(double number, int length)
+    public static double[]
+    MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Step 1: Create a new array of given length
+        double[]
+        result = new double[length];
 
-        return []; // replace this return statement with your own
+        // Step 2: Fill array with multiples of the number
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = (i + 1) * number;
+        }
+
+        // Step 3: Return the filled array
+        return result;
     }
 
     /// <summary>
-    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
-    /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
-    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
-    ///
-    /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
+    /// Rotates the 'data' list to the right by 'amount', in-place (without using extra lists).
+    /// For example, rotating List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} by 3 results in:
+    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}
     /// </summary>
-    public static void RotateListRight(List<int> data, int amount)
+    public static void
+    RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        int n = data.Count;
+
+        // Normalize amount in case it's greater than list size
+        amount = amount % n;
+
+        // Reverse the entire list
+        Reverse(data, 0, n - 1);
+
+        // Reverse the first 'amount' elements
+        Reverse(data, 0, amount - 1);
+
+        // Reverse the remaining elements
+        Reverse(data, amount, n - 1);
+    }
+
+    // Helper method to reverse part of a list in-place
+    private static void
+    Reverse(List<int> list, int start, int end)
+    {
+        while (start < end)
+        {
+            int temp = list[start];
+            list[start] = list[end];
+            list[end] = temp;
+
+            start++;
+            end--;
+        }
     }
 }
+
+
